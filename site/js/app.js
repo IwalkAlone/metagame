77b494b305499103cd92a496912cb30117815f19
Sergey Skrynnikov <minotaar.hh@gmail.com>
2014-07-15 22:04:16 +0400

    Initial Commit

---
+++ site/js/app.js
@@ -0,0 +1,6 @@
+﻿'use strict';
+angular
+    .module('main', [])
+    .run(function () {
+
+    });
---
+++ site/js/mainModule.js
@@ -0,0 +1,5 @@
+﻿angular
+    .module('main')
+    .controller('testController', ['$scope', function ($scope) {
+        $scope.hello = "Hello";
+    }]);
