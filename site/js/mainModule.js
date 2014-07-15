9720d355f1f79a5547b59d8d9033d9765be25437
Sergey Skrynnikov <minotaar.hh@gmail.com>
2014-07-15 23:10:53 +0400

    Interface for editing matchups

--- site/js/mainModule.js
+++ site/js/mainModule.js
@@ -2,11 +2,28 @@
     .module('main')
     .controller('deckInputController', ['$scope', function ($scope) {
         $scope.decks = [];
+
+        var id = 0;
+
+        var nextId = function () {
+            id++;
+            return id;
+        };
+
         $scope.addDeck = function () {
-            $scope.decks.push({
+            var newDeck = {
+                id: nextId(),
                 name: '',
-                share: ''
+                share: '',
+                matchups: {}
+            };
+
+            _.each($scope.decks, function (deck) {
+                newDeck.matchups[deck.id] = 50;
+                deck.matchups[newDeck.id] = 50;
             });
+
+            $scope.decks.push(newDeck);
         };
 
         $scope.selectDeck = function (deck) {
@@ -15,5 +32,15 @@
 
         $scope.deckIsSelected = function (deck) {
             return $scope.selectedDeck === deck;
+        };
+
+        $scope.unselectedDecks = function () {
+            return _.filter($scope.decks, function (deck) {
+                return deck !== $scope.selectedDeck;
+            })
         }
+
+        $scope.syncMatchup = function (deck, otherDeck) {
+            otherDeck.matchups[deck.id] = 100 - deck.matchups[otherDeck.id];
+        };
     }]);
